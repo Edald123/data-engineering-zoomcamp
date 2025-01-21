8e8b6e5378e13fac547b74f74c73d7ae20beab35
Eduardo Ahumada <eduardo.ahumadagj@gmail.com>
2025-01-20 22:24:57 -0600

    added dockerfile

---
+++ 01-docker-terraform/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+
+print(sys.argv)  # command line arguments we pass to the script
+
+day = sys.argv[1]  # argument 1 is the information we pass
+
+# Some logic with pandas
+
+print(f'job finished successfully for day = {day}')
